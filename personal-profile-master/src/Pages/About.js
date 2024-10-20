1b836f0c80d94216e8cbf909b315c2fd3ad591e6
NavarrozaYvan <navarrozayvanjade52@gmail.com>
2024-10-20 22:47:49 +0800

    About.js

--- personal-profile-master/src/Pages/About.js
+++ personal-profile-master/src/Pages/About.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css'; 
 import './styles.css';
 
 const About = () => {
-  return (
-    <div className="text-center page-container">
-      <h1 className="display-4 mb-4">About Me</h1>
-      <p>I am a 3rd Year Computer Science student at Pamantasan ng Cabuyao. My hobbies include editing, photography, videography, graphic designing, and playing drums. I want to explore and learn more skills in both my hobbies and my course.</p>
-      <div className="d-flex flex-column">
-        <Link to="/" className="btn btn-outline-light btn-lg my-2 animate-button">Go to Home</Link>
-      </div>
-    </div>
-  );
-};
+    return(
+        <div>
+            <h1>About Me</h1>
+            <p>I am a 3rd Year Computer Science student at Pamantasan ng Cabuyao. 
+            My hobbies include editing, photography, videography, graphic designing, and playing drums. I want to explore and learn more skills 
+            in both my hobbies and my course.</p>
 
-export default About;
+        </div>
+    )
+}
+export default About
